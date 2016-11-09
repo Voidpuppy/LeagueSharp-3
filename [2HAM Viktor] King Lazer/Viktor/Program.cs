75f64386f30b9ddf081d229d918beccd97e70fd3
TwoHam <lourelucas@gmail.com>
2016-11-09 19:29:39 -0200

    Viktor
    Release Viktor

---
+++ [2HAM Viktor] King Lazer/Viktor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace Viktor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            switch (ObjectManager.Player.ChampionName)
+            {
+                case "Viktor":
+                    new Champions.Viktor();
+                    Game.PrintChat("<font color='#FFFF33'>[2HAM Viktor] Successfully Loaded</font>");
+                    break;
+                default:
+                    Game.PrintChat("[2HAM Viktor] Your Champion is not Viktor");
+                    break;
+            }
+        }
+    }
+}
